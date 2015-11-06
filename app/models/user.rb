791301bea8e3467db3e8351966f31f065cec566b
Vladimir Hale <vladimir.hale@gmail.com>
2015-11-05 23:28:27 -0600

    Needs seeding

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ActiveRecord::Base
+	has_one :profile
+	has_many :todo_lists, dependent: :destroy
+	has_many :todo_items, through: :todo_lists, source: :todo_items
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.destroy_all
+Profile.destroy_all
+
+User.create! [
+  { username: "fiorina", password_digest: "fiorina123" },
+  { username: "trump", password_digest: "trump123" },
+  { username: "carson", password_digest: "carson123" },
+  { username: "clinton", password_digest: "clinton123" }
+]
+
+
+Profile.create! [
+  { first_name: "Carly", last_name: "Fiorina", birth_year: 1954, gender: "female" },
+  { first_name: "Donald", last_name: "Trump", birth_year: 1946, gender: "male" },
+  { first_name: "Ben", last_name: "Carson", birth_year: 1951, gender: "male" },
+  { first_name: "Hillary", last_name: "Clinton", birth_year: 1947, gender: "female" }
+]
