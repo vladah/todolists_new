ec3c6e1b282514cbff0e195ab8dc59d21b17cfc0
Vladimir Hale <vladimir.hale@gmail.com>
2015-11-06 20:19:45 -0600

    All tests passed

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,22 @@
 class Profile < ActiveRecord::Base
   belongs_to :user
+
+  validates :gender, inclusion: ['male', 'female']
+  validate :first_or_last
+  validate :males_no_sue
+
+  def first_or_last
+  	if first_name == nil and last_name == nil
+      errors.add(:first_name, "First name and last name cannot both be empty")
+      errors.add(:last_name, "First name and last name cannot both be empty")
+    end
+  end
+
+  def males_no_sue
+  	errors.add(:first_name, "If you are a male you cannot be called Sue, lol.") if gender == "male" and first_name == "Sue"
+  end
+  
+  def self.get_all_profiles(min_by, max_by)
+  	Profile.where("birth_year BETWEEN ? AND ?", min_by, max_by).order("birth_year ASC")
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
-	has_one :profile
+	has_one :profile, dependent: :destroy
 	has_many :todo_lists, dependent: :destroy
-	has_many :todo_items, through: :todo_lists, source: :todo_items
+	has_many :todo_items, through: :todo_lists, source: :todo_items, dependent: :destroy
+
+	validates :username, presence: true
+
+	def get_completed_count(user)
+		user.todo_items.where(["completed == ?", true]).count
+    end
 end
